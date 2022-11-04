7ef3de0e5c611cf7e05e80aa22bc5a0f8a696805
tanishqkrk <tanishqkrk@gmail.com>
2022-11-04 13:31:27 +0530

    removed console.logs

--- random.js
+++ random.js
@@ -2,15 +2,15 @@
 
 const random = {
     number: function () {
-        console.log((Math.random()));
+        // console.log((Math.random()));
         return (Math.random());
     },
     numberFromZero: function (limit) {
-        console.log(Math.floor(Math.random() * (limit)));
+        // console.log(Math.floor(Math.random() * (limit)));
         return Math.floor(Math.random() * (limit));
     },
     numberRange: function (from, to) {
-        console.log(Math.floor(Math.random() * (to) + (from)));
+        // console.log(Math.floor(Math.random() * (to) + (from)));
         return Math.floor(Math.random() * (to) + (from));
     },
     uLetters: function (amount) {
@@ -20,7 +20,7 @@ const random = {
             newLetter = letters[Math.floor(Math.random() * letters.length + 0)];
             outputArray.push(newLetter);
         }
-        console.log(outputArray);
+        // console.log(outputArray);
         return outputArray;
     },
     sLetters: function (amount) {
@@ -30,11 +30,11 @@ const random = {
             newLetter = letters[Math.floor(Math.random() * letters.length + 0)];
             outputArray.push(newLetter);
         }
-        console.log(outputArray);
+        // console.log(outputArray);
         return outputArray
     },
     element: function (array) {
-        console.log(array[Math.floor(Math.random() * array.length + 0)]);
+        // console.log(array[Math.floor(Math.random() * array.length + 0)]);
         return array[Math.floor(Math.random() * array.length + 0)];
     },
     elements: function (array, amount) {
@@ -43,28 +43,28 @@ const random = {
             newElement = array[Math.floor(Math.random() * array.length + 0)];
             outputArray.push(newElement);
         }
-        console.log(outputArray);
+        // console.log(outputArray);
         return outputArray;
     }
 }
 
-random.number()
+// random.number()
 // Generates a random number between 0 and 1
 
-random.numberFromZero(100)
+// random.numberFromZero(100)
 // Generates a random number from 0 to the limit provided
 
-random.numberRange(0, 10)
+// random.numberRange(0, 10)
 // Generates a random number between given range
 
-random.uLetters(1)
+// random.uLetters(1)
 // Generates a random Uppercase letter
 
-random.sLetters(1)
+// random.sLetters(1)
 // Generates a random lowercase letter
 
-random.element([1, 2, 3, 4])
+// random.element([1, 2, 3, 4])
 //Returns a random element from array
 
-random.elements([1, 2, 3, 4], 10)
+// random.elements([1, 2, 3, 4], 10)
 //Returns random elements from array
